ac89d22f27f8f5ec6b2e7f862780dd37df5f80ba
Matt Nickodemus <matthewnickodemus@gmail.com>
2022-07-06 08:23:27 -0600

    added development notes

---
+++ dev/dev.R
@@ -0,0 +1,8 @@
+# This file should be used to keep track of your development throughout the project.
+
+
+# Add Dev Package Dependency ####
+# install.packages("devtools")
+devtools::install_github("dsu-effectiveness/utValidateR")
+
+usethis::use_dev_package("utValidateR")

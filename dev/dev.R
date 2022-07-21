d5b0ce9783f89c953acd2fd25970cbe5abef696e
Matt Nickodemus <matthewnickodemus@gmail.com>
2022-07-21 16:44:23 -0600

    added the fake course validation data set

--- dev/dev.R
+++ dev/dev.R
@@ -21,3 +21,5 @@ usethis::use_data_raw(name = 'fake_graduation_validation', open = FALSE)
 usethis::use_data_raw(name = 'fake_buildings_validation', open = FALSE)
 
 usethis::use_data_raw(name = 'fake_rooms_validation', open = FALSE)
+
+usethis::use_data_raw(name = 'fake_course_validation', open = FALSE)

be459454dbc14b569bf659f836d90247e7b898d8
Mark Hagemann <mhagemann@eab.com>
2022-07-25 13:25:17 -0400

    Use tryCatch for condition handling in make_checker()

--- R/checker.R
+++ R/checker.R
@@ -11,29 +11,24 @@ make_checker <- function(expr) {
   expr_chr <- deparse(expr)
 
   outfun <- function(df) {
-    out2 <- try(eval_tidy(expr, df), silent = TRUE)
-    if (inherits(out2, "try-error")) {
-      # TODO: this could be simpler and more general with a tryCatch
-      warning("Check attempt failed for ", expr_chr, "\n",
+
+    tryCatch({
+      out2 <- eval_tidy(expr, df)
+
+      assert_no_missing(out2, expr_chr)
+      assert_logical(out2, expr_chr)
+      assert_length(out2, df, expr_chr)
+      out2
+    },
+
+    error = function(cond) {
+      message("Check attempt failed for ", expr_chr, "\n",
               "With the following error:\n",
-              get_tryerror(out2), "\n\n")
-
+              cond, "\n\n")
       return(rep(FALSE, nrow(df)))
-    }
+    })
 
-    assert_no_missing(out2, expr_chr)
-    assert_logical(out2, expr_chr)
-    assert_length(out2, df, expr_chr)
-
-    out2
   }
-  outfun
-}
 
-#' Returns the text of the error message in a `try()` failure
-#'
-#' @param tryerror result of `try()` failure
-get_tryerror <- function(tryerror) {
-  cond <- attr(tryerror, "condition")
-  cond$message
+  outfun
 }

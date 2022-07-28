c2d86ebf9d33c3f92f8338ac4cb11256839e1b66
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2022-07-28 12:07:44 -0600

    added additional columns to the buildings validation sql

--- sandbox/buildings/sql/buildings_validation.sql
+++ sandbox/buildings/sql/buildings_validation.sql
@@ -11,7 +11,13 @@ SELECT a.building_location_code,
        a.building_cost_myr,
        a.building_number,
        a.building_auxiliary,
-       a.building_activity_date
+       a.building_activity_date,
+       building_number,
+       -- need to add to fake data
+       a.building_name,
+       a.building_abbrv,
+       a.building_number as building_risk_number,
+       a.building_ownership_code
 FROM quad.buildings a
 WHERE is_state_reported
  AND (building_to_term_id IS NULL OR building_to_term_id > (SELECT DISTINCT term_id FROM quad.term WHERE is_current_term))

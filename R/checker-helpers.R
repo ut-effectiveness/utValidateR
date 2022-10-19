e03a7c2038b4e423f36949f2b1523de379831489
Justin Pfeifer <justinpfeifer@Justins-MacBook-Pro.local>
2022-10-19 10:39:20 -0600

    Added additional US states

--- R/checker-helpers.R
+++ R/checker-helpers.R
@@ -333,11 +333,11 @@ is_utah_county <- function(county_code) {
 #' @param state first_admit_state_code
 #' @export
 is_us_state <- function(state) {
-  us_states <- c("AL", "AK", "AZ", "AE", "AP", "AR", "AS", "CA", "CO", "CT", "DE", "DC", "FL",
-                 "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-                 "MA", "MI", "MN", "MS", "MO", "MP", "MT", "NE", "NV", "NH", "NJ", "NM",
-                 "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN",
-                 "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
+  us_states <- c("AA", "AE", "AK", "AL", "AP", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL",
+                 "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME",
+                 "MH", "MI", "MN", "MS", "MO", "MP", "MT", "NE", "NV", "NH", "NJ", "NM",
+                 "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN",
+                 "TX", "UT", "VA", "VI", "VT", "WA", "WV", "WI", "WY")
   state %in% us_states
 }
 

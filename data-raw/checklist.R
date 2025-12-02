8c1ee335229b39caffdcf439beccba503ef6c023
mkhassan-isu <mhassan.isu@gmail.com>
2025-12-02 10:26:45 -0700

    Resolving issue #125

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -224,6 +224,7 @@ rule_spec <- tribble(
         !(schedule_code %in% c("LEC", "LEX", "LEL")) & !is.na(lecture_hours) & lecture_hours > 0)),
   "UTC09", expr(!(subject_code != "CED" & !is.na(enrollment) & enrollment != 0 &
         !(schedule_code %in% c("LAB", "LBC", "LEL", "ACT")) & !is.na(lab_hours) & lab_hours > 0)),
+  "UTC10", expr(!(is.na(program_type) & subject_code != "CED")),
   "C13", expr(is_valid_values(program_type, valid_program_types, missing_ok = TRUE)),
   "C13a", expr(TODO("USHE check on perkins program types. Requires a query?")),
   "C13c", expr(TODO("USHE check on perkins budget codes. Need query for perkins codes?")),

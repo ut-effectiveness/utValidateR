3c3bc99f294af2ee1b1e2ae1f338a79acc8b82e4
mkhassan-isu <mhassan.isu@gmail.com>
2025-12-22 22:24:22 -0700

    Resolving issue #135

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -510,6 +510,7 @@ rule_spec <- tribble(
   "UTS22", expr(!(((us_citizenship_code != "2" & !is.na(visa_type)) | (us_citizenship_code == "2" & is.na(visa_type)) |
           (!us_citizenship_code %in% c("2","3") & !is.na(visa_type)) | (us_citizenship_code == "2" & is.na(visa_type))) &
         (visa_expire_date > Sys.Date() | is.na(visa_expire_date)))),
+  "UTS23", expr(!(is_missing_chr(cur_prgm) & level_id != "NC")),
 
   "UTG01", expr(as.numeric(substr(graduated_term_id, 1, 4)) == as.numeric(lubridate::year(graduation_date))),
   "UTG02", expr(as.numeric(graduated_academic_year_code) == as.numeric(graduation_academic_year_check)),

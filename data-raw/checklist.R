9e6f5aab860702ef924e9f93a2d35bfc7d88d917
mkhassan-isu <mhassan.isu@gmail.com>
2026-02-12 18:48:58 -0700

    Resolving C12a and C33a

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -268,11 +268,11 @@ rule_spec <- tribble(
          !(instruction_method_code %in% c("C","I","V","Y")) & (section_format_type_code %in% c("LEC","LEL","LAB")) &
          (budget_code != "SF") & (room_use_code_1 %in% c("110","210")) & (version_id == "3"))),
   "C25a", expr(utValidateR::is_missing_chr(trimws(meet_days_2)) | !utValidateR::is_missing_chr(trimws(meet_days_1)) |
-      (campus_id == "V")) , # USHE check, TODO: add site-type (query) condition?
+      (c_site_type == "V")) , # USHE check, TODO: add site-type (query) condition?
   "C33a", expr(!(!(is.na(trimws(meet_days_3)) | trimws(meet_days_3) == "") &
       ((is.na(trimws(meet_days_2)) | trimws(meet_days_2) == "") |
           (is.na(trimws(meet_days_1)) | trimws(meet_days_1) == "")) &
-      !(campus_id == "V" & meet_site_type_2 == "V"))), # USHE check, TODO: the rule s not showing up. in the app
+      !(c_site_type == "V" & c_site_type2 == "V"))), # USHE check, TODO: the rule s not showing up. in the app
   "C18", expr(is.na(meet_building_id_1) | !equivalent(meet_building_id_1, building_number_1)),
   "C26", expr(is.na(meet_building_id_2) | !equivalent(meet_building_id_2, building_number_2)),
   "C34", expr(is.na(meet_building_id_3) | !equivalent(meet_building_id_3, building_number_3)),

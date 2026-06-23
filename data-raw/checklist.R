610d1ea6f310096e47e365b0e79f65933aa1f64e
mkhassan-isu <mhassan.isu@gmail.com>
2026-06-22 18:18:54 -0600

    Fixing P05a, incluiding P05b

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -539,7 +539,8 @@ rule_spec <- tribble(
   "P03a", expr(!(is_missing_chr(cip_code) | cip_code %in% c("000000", "005000"))),
   "P03b", expr(cip_code %in% valid_cip_codes),
   "P04a", expr(!is_missing_chr(ipeds_award_level_code) & ipeds_award_level_code %in% valid_ipeds_award_levels_prog),
-  "P05A", expr(!(ipeds_award_level_code %in% c("3", "5") & is_missing_chr(pf_degree_type)))
+  "P05a", expr(!(ipeds_award_level_code %in% c("3", "5") & is_missing_chr(pf_degree_type))),
+  "P05b", expr(!(!(ipeds_award_level_code %in% c("3", "5")) & is_missing_chr(pf_degree_type)))
 )
 
 

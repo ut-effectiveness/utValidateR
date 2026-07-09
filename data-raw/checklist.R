94db5f7589396f0db0dbc063d178239bcbb6a7dd
mkhassan-isu <mhassan.isu@gmail.com>
2026-07-09 17:42:36 -0600

    Fixing PF08a typo

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -552,7 +552,7 @@ rule_spec <- tribble(
   "PF07b", expr(pf_te %in% c("N", "Y")),
   "PF07c", expr(!(pf_te == "Y" & !(c_inst %in% c("3677", "3679", "5220", "5221")))), #using c_inst in place of pf_inst
   "PF07d", expr( pf_te != "Y" | ipeds_award_level_code %in% c("1A", "1B", "2")),
-  "PF08A", expr(is_valid_pf_credit_hours(pf_credit_hours))
+  "PF08a", expr(is_valid_pf_credit_hours(pf_credit_hours))
 )
 
 

e7df8842bee1e49c51b1b40a5c3798de1e0524d4
mkhassan-isu <mhassan.isu@gmail.com>
2026-09-07 00:53:43 -0600

    updated C24a to resolve issue reagrding missing instructors

--- data-raw/checklist.R
+++ data-raw/checklist.R
@@ -323,7 +323,7 @@ rule_spec <- tribble(
   "C41d", expr(is_missing_chr(c_title) |
                  !grepl("[^a-zA-Z0-9 /&()+:.-\\']", c_title) |
                  !(c_extract %in% "3")),
-  "C42a", expr(!is_missing_chr(instructor_employee_id) | (class_size == 0)),
+  "C42a", expr(!is_missing_chr(instructor_employee_id)),
   "C42b", expr(!is_missing_chr(instructor_employee_id) |
                  (class_size == 0) |
                  (nchar(instructor_employee_id) == 8 &
